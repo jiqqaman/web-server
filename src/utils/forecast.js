563af966f67d16a74c3a81e41f29713c92852439
Jose <jiqqaman2@gmail.com>
2020-04-01 18:24:11 -0400

    Added high and low to forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lat, long, callback) => {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, 
-                body.daily.data[0].summary + ' It is currently ' + body.currently.temperature +' degrees out. There is a '+ body.currently.precipProbability +'% chance of rain'
+                body.daily.data[0].summary + ' It is currently ' + body.currently.temperature +' degrees out. There is a '+ body.currently.precipProbability +'% chance of rain with a high of ' + body.daily.data[0].temperatureHigh + ' degrees and a low of ' + body.daily.data[0].temperatureLow + ' degrees.' 
             )
         }
     })
